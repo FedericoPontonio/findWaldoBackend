48e1539b30078c59b5ce981fbaa2f6201589e4c4
federico <pontonio.federico@gmail.com>
2025-01-22 15:16:05 +0100

    Enables cors

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const app = express()
 const router = require('./router')
 
 app.use(express.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
 app.use('/', router);
 
 app.listen(3000)
